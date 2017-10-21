0b87d6a3d6a5759291c68452a24ff09cd62dd5ef
Bhushan Goel <bhushan.goel@paytm.com>
2017-10-21 13:56:33 +0530

    trees

--- trees.py
+++ trees.py
@@ -36,18 +36,116 @@ class Tree:
 
 root = Tree(1)
 
+root.left = Tree(2)
+root.right = Tree(3)
 
-# root.left = Tree(2)
-# root.right = Tree(3)
-#
-# root.left.left = Tree(4)
-# root.left.right = Tree(5)
+root.left.left = Tree(4)
+root.left.right = Tree(5)
 
-# root.right.left = Tree(6)
-# root.right.right = Tree(7)
+root.right.left = Tree(6)
+root.right.right = Tree(7)
+
+root.left.left.left = Tree(11)
+
+
+# root.left.left.right = Tree(6)
+# root.left.left.right.left = Tree(7)
+
 
 # print(root)
 # print(root.printAllLevelOrder())
+
+def deleteNode(root, data):
+    if root is None:
+        return
+    q = Queue()
+    node = None
+    deep = None
+    dleft = None
+    dright = None
+    result = []
+    q.enqueue(root)
+
+    while not q.isEmpty():
+        node = q.dequeue()
+        result.append(node.data)
+        if node.getLeft():
+            q.enqueue(node.getLeft())
+            if node.getLeft().data == data:
+                dleft = node
+        if node.getRight():
+            q.enqueue(node.getRight())
+            if node.getRight().data == data:
+                dright = node
+
+    root.printAllLevelOrder()
+    deep = node
+    if dleft is not None:
+        dleft.left = deep
+        node.data = data
+    elif dright is not None:
+        dright.right.data, node.data = deep.data, data
+
+    print("node here : ", deep, deep.data)
+    deep = None
+    root.printAllLevelOrder()
+
+
+# deleteNode(root, 3)
+
+
+def depeestNode(root):
+    if root is None:
+        return
+    q = Queue()
+    node = None
+    q.enqueue(root)
+
+    while not q.isEmpty():
+        node = q.dequeue()
+        if node.getLeft():
+            q.enqueue(node.getLeft())
+        if node.getRight():
+            q.enqueue(node.getRight())
+
+    return node.data
+
+
+# depeestNode(root)
+
+'''
+Iterative method
+count number of levels in tree and return height accordingly
+'''
+
+
+def getHeight(root):
+    if root is None:
+        return 0
+
+    q = []
+    node = None
+    height = 0
+    q.append(root)
+
+    while True:
+        # number of nodes at current level
+        l = len(q)
+        if l == 0:
+            return height - 1
+        height += 1
+        while l > 0:
+            node = q[0]
+            q.pop(0)
+            if node.getLeft():
+                q.append(node.getLeft())
+            if node.getRight():
+                q.append(node.getRight())
+            l -= 1
+
+
+# print(getHeight(root))
+
 
 def insertItem(root, item):
     if root is None:
@@ -79,19 +177,23 @@ def insertItem(root, item):
     root.printAllLevelOrder()
 
 
-insertItem(root, 5)
+# insertItem(root, 5)
 
 
 def deleteTree(root):
+    # print("root 1 : ", root.printAllLevelOrder())
     if root is None:
         return
-    root.printAllLevelOrder()
+    # root.printAllLevelOrder()
     deleteTree(root.left)
     deleteTree(root.right)
+    root.printAllLevelOrder()
     del root
 
 
-# deleteTree(root)
+
+
+deleteTree(root)
 
 
 def reverseTree(root):
